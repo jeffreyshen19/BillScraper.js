9273159ee08e1044cfdf9e1fbc147257cdd06a3c
jeffreyshen19 <jshen@andover.edu>
2018-01-24 22:25:02 -0500

    added unit tests for getBill

--- test/getBill.test.js
+++ test/getBill.test.js
@@ -1,14 +1,37 @@
+/*jshint esversion: 6*/
+
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 var getBill = require("../lib/scraper/getBill.js");
 
-/*describe('getBill', function() {
-  it('should be a string', function() {
-    expect(getBill(1, (res) => {})).to.throw(TypeError);
+describe('getBill', function() {
+  it('should throw an error if billName is not a string', function() {
+    expect(() => {
+      getBill(1, (res) => {});
+    }).to.throw(TypeError);
   });
-});
-*/
 
-getBill("BILLSTATUS-115sres99", function(){
-  
+  it('should throw an error if there is no callback', function() {
+    expect(() => {
+      getBill("");
+    }).to.throw(TypeError);
+  });
+
+  it('should throw an error if the billName is not valid', function() {
+    expect(() => {
+      getBill("", (res) => {});
+    }).to.throw(TypeError);
+  });
+
+  it('should throw an error if the billName is not valid', function() {
+    expect(() => {
+      getBill("", (res) => {});
+    }).to.throw(TypeError);
+  });
+
+  it('should return an object if the billName is valid', function() {
+    getBill("BILLSTATUS-115sres99", (res) => {
+      expect(res).to.be.an("object").that.is.not.empty;
+    });
+  });
 });
