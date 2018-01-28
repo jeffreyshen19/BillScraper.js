e436676903c5f16ba42fef9c7c350b9cfce88129
jeffreyshen19 <jshen@andover.edu>
2018-01-28 10:25:17 -0500

    added a field called validBillTypes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   getBulk: require("./lib/scraper/getBulk.js"),
   getBill: require("./lib/scraper/getBill.js"),
-  parseBill: require("./lib/parser/parseBill.js")
+  parseBill: require("./lib/parser/parseBill.js"),
+  validBillTypes: ["sres", "sjres", "sconres", "s", "hres", "hr", "hjres", "hconres"]
 };
