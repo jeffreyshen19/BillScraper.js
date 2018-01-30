160d801e6bf02b9f0d8562bc881b9c16cffc9b50
jeffreyshen19 <jshen@andover.edu>
2018-01-30 14:45:38 -0500

    added a file getBill.js which scrapes the actual bill text from congress

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 module.exports = {
+  //Status
   getBulkStatus: require("./lib/scraper/getBulkStatus.js"),
   getBillStatus: require("./lib/scraper/getBillStatus.js"),
   cleanBillStatus: require("./lib/parser/cleanBillStatus.js"),
+
+  //Bill
+  getBill: require("./lib/scraper/getBill.js"),
+
+  //Fields
   validBillTypes: ["sres", "sjres", "sconres", "s", "hres", "hr", "hjres", "hconres"]
 };
---
+++ runner/getBill.js
@@ -0,0 +1,5 @@
+var billScraper = require("../");
+
+billScraper.getBill("BILLS-115hr1001ih", function(res){
+
+});
