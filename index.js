8d0d25538417c4f1d53bf5d01608db7df98e9d48
jeffreyshen19 <jshen@andover.edu>
2018-01-30 14:03:44 -0500

    renamed status files for clarity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  getBulk: require("./lib/scraper/getBulk.js"),
-  getBill: require("./lib/scraper/getBill.js"),
-  cleanBill: require("./lib/parser/cleanBill.js"),
+  getBulkStatus: require("./lib/scraper/getBulkStatus.js"),
+  getBillStatus: require("./lib/scraper/getBillStatus.js"),
+  cleanBillStatus: require("./lib/parser/cleanBillStatus.js"),
   validBillTypes: ["sres", "sjres", "sconres", "s", "hres", "hr", "hjres", "hconres"]
 };
