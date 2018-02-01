1def2e2dd834857de60894f9d2b7e09f3b06780a
jeffreyshen19 <jshen@andover.edu>
2018-02-01 11:19:04 -0500

    Daily Commit 2/1/18 Added unit tests, updated docs (in gh-pages), added more error handlers to each file, added a method getBulk.js which access the names of all bill files

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 module.exports = {
-  //Status
-  getBulkStatus: require("./lib/scraper/getBulkStatus.js"),
-  getBillStatus: require("./lib/scraper/getBillStatus.js"),
-  cleanBillStatus: require("./lib/parser/cleanBillStatus.js"),
+  // **Scraper**
+    //Status
+    getBulkStatus: require("./lib/scraper/getBulkStatus.js"),
+    getBillStatus: require("./lib/scraper/getBillStatus.js"),
 
-  //Bill
-  getBulk: require("./lib/scraper/getBulk.js"),
-  getBill: require("./lib/scraper/getBill.js"),
-  cleanBill: require("./lib/parser/cleanBill.js"),
-  printBill: require("./lib/parser/printBill.js"),
+    //Bill
+    getBulk: require("./lib/scraper/getBulk.js"),
+    getBill: require("./lib/scraper/getBill.js"),
+    
+  // **Parser**
+    //Status
+    cleanBillStatus: require("./lib/parser/cleanBillStatus.js"),
+
+    //Bill
+    cleanBill: require("./lib/parser/cleanBill.js"),
+    printBill: require("./lib/parser/printBill.js"),
 
   //Fields
   validBillTypes: ["sres", "sjres", "sconres", "s", "hres", "hr", "hjres", "hconres"]
