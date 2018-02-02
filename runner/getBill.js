65d2ee570f9d3e2934c84dbc7e1258209edec62b
jeffreyshen19 <jshen@andover.edu>
2018-02-02 11:09:26 -0500

    Daily Commit 2/2/18 Added a method printBill which recursively goes through a cleaned bill and generates a single block of text

--- runner/getBill.js
+++ runner/getBill.js
@@ -3,6 +3,6 @@ var util = require("util");
 
 billScraper.getBill("BILLS-113hr1033rs", function(res){
   billScraper.printBill(res, function(parsedResult){
-    console.log(util.inspect(parsedResult, false, null));
+    //console.log(util.inspect(parsedResult, false, null));
   });
 }, {session: 2});
