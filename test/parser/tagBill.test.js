cc6dd15b6183c09ba9f3555b00bcfcb6dc707bb3
jeffreyshen19 <jshen@andover.edu>
2018-02-07 13:38:50 -0500

    testing travis CI

---
+++ test/parser/tagBill.test.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 6*/
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var printBill = require("../../lib/parser/printBill.js");
+var tagBill = require("../../lib/parser/tagBill.js");
+var getBill = require("../../lib/scraper/getBill.js"); 
+
+describe('tagBill', function() {
+  it('should throw an error if bill is not an object', function() {
+    expect(() => {
+      tagBill(1, (res) => {});
+    }).to.throw(TypeError);
+  });
+
+  it('should throw an error if there is no callback', function() {
+    expect(() => {
+      tagBill({});
+    }).to.throw(TypeError);
+  });
+
+  it('should return an object if the bill is valid', function(){
+    getBill("BILLS-113hr1033rs", function(res){
+      printBill(res, function(parsedResult){
+        tagBill(parsedResult, function(tags){
+          expect(tags).to.be.an("object").that.is.not.empty;
+       });
+     });
+    }, {session: 2});
+  });
+});
