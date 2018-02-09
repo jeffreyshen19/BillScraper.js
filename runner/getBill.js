dc91a308a4ee179c7e86d63dcf3952ac2e5cf764
jeffreyshen19 <jshen@andover.edu>
2018-02-09 12:58:02 -0500

    removed files

--- runner/getBill.js
+++
@@ -1,8 +0,0 @@
-var billScraper = require("../");
-var util = require("util");
-
-billScraper.getBill("BILLS-113hr1033rs", function(res){
-  billScraper.printBill(res, function(parsedResult){
-    //console.log(util.inspect(parsedResult, false, null));
-  });
-}, {session: 2});
