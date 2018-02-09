54e55b16fa8cac993be5e1287503754e63118d84
jeffreyshen19 <jshen@andover.edu>
2018-02-09 00:06:38 -0500

    refactored billScraper as an object

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-module.exports = {
-  // **Scraper**
-    //Status
-    getBulkStatus: require("./lib/scraper/getBulkStatus.js"),
-    getBillStatus: require("./lib/scraper/getBillStatus.js"),
+var mongoose = require('mongoose');
 
-    //Bill
-    getBulk: require("./lib/scraper/getBulk.js"),
-    getBill: require("./lib/scraper/getBill.js"),
+var billScraper = function(){
+  //Scraper
+  this.getBulkStatus = require("./lib/scraper/getBulkStatus.js");
+  this.getBillStatus = require("./lib/scraper/getBillStatus.js");
+  this.getBulk = require("./lib/scraper/getBulk.js");
+  this.getBill = require("./lib/scraper/getBill.js");
 
-  // **Parser**
-    //Status
-    cleanBillStatus: require("./lib/parser/cleanBillStatus.js"),
+  //Parser
+  this.cleanBillStatus = require("./lib/parser/cleanBillStatus.js");
+  this.cleanBill = require("./lib/parser/cleanBill.js");
+  this.printBill = require("./lib/parser/printBill.js");
+  this.tagBill = require("./lib/parser/tagBill.js");
 
-    //Bill
-    cleanBill: require("./lib/parser/cleanBill.js"),
-    printBill: require("./lib/parser/printBill.js"),
-    tagBill: require("./lib/parser/tagBill.js"),
-
-  //Fields
-  validBillTypes: ["sres", "sjres", "sconres", "s", "hres", "hr", "hjres", "hconres"]
+  //Constants
+  this.validBillTypes = ["sres", "sjres", "sconres", "s", "hres", "hr", "hjres", "hconres"];
 };
+
+module.exports = billScraper;
