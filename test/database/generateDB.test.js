e59ca734d9609574ba1595d3ecfad78a6b144c27
jeffreyshen19 <jshen@andover.edu>
2018-02-11 16:41:15 -0500

    added structure for createBill and generateDB unit tests

---
+++ test/database/generateDB.test.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6*/
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var cleanBillStatus = require("../../lib/parser/cleanBillStatus");
+var getBillStatus = require("../../lib/scraper/getBillStatus");
+
+describe('generateDB', function() {
+
+});
