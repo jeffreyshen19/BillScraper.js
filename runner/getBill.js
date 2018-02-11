b6698b9889801eb98ae1af6a33acc86b6427a710
jeffreyshen19 <jshen@andover.edu>
2018-02-11 13:35:58 -0500

    generateDB works

--- runner/getBill.js
+++ runner/getBill.js
@@ -1,8 +1,11 @@
 var billScraper = require("../");
 var util = require("util");
 
-billScraper.getBill("BILLS-113hr1033rs", function(res){
+/*billScraper.getBill("BILLS-113hr1033rs", function(res){
   billScraper.printBill(res, function(parsedResult){
     console.log(util.inspect(parsedResult, false, null));
   });
-}, {session: 2});
+}, {session: 2});*/
+
+console.log(billScraper.getBillSync("BILLS-113hr1033rs", {session: 2}));
+console.log("method finished");
