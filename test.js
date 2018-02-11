2fbb6dee2ab9d4a3353defbeb4150604dd7aa374
jeffreyshen19 <jshen@andover.edu>
2018-02-11 17:05:21 -0500

    removed test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var billScraper = require("./");
-
-billScraper.getBill("BILLS-113hr1033rs", function(res){
- billScraper.printBill(res, function(parsedResult){
-   billScraper.tagBill(parsedResult, function(tags){
-      console.log(tags);
-   });
- });
-}, {session: 2});
