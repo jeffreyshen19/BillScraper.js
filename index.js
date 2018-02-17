87d264747df8e5936915f3ec11c2639aea3f0f95
jeffreyshen19 <jshen@andover.edu>
2018-02-16 21:42:44 -0500

    refactor index.js

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 module.exports = {
-  // **Database**
-    createBill: require("./lib/database/createBill"),
-    generateDB: require("./lib/database/generateDB"),
-    Bill: require("./lib/database/models/Bill"),
+  /*
+    DATABASE
+    Methods that aid in generating the database of bills
+  */
+  createBill: require("./lib/database/createBill"),
+  generateDB: require("./lib/database/generateDB"),
+  Bill: require("./lib/database/models/Bill"),
 
-  // **Scraper**
-    //Status
-    getBulkStatus: require("./lib/scraper/getBulkStatus"),
-    getBillStatus: require("./lib/scraper/getBillStatus"),
+  /*
+    SCRAPER
+    Methods that grab data from the server
+  */
+  //Get Statuses
+  getBulkStatus: require("./lib/scraper/getBulkStatus"),
+  getBillStatus: require("./lib/scraper/getBillStatus"),
 
-    //Bill
-    getBulk: require("./lib/scraper/getBulk"),
-    getBill: require("./lib/scraper/getBill"),
+  //Get Bills
+  getBulk: require("./lib/scraper/getBulk"),
+  getBill: require("./lib/scraper/getBill"),
 
-  // **Parser**
-    //Status
-    cleanBillStatus: require("./lib/parser/cleanBillStatus"),
+  /*
+    PARSER
+    Methods that clean up and analyze the raw data grabbed from the server
+  */
+  //Clean statuses
+  cleanBillStatus: require("./lib/parser/cleanBillStatus"),
 
-    //Bill
-    cleanBill: require("./lib/parser/cleanBill"),
-    printBill: require("./lib/parser/printBill"),
-    tagBill: require("./lib/parser/tagBill"),
+  //Work with bills
+  cleanBill: require("./lib/parser/cleanBill"),
+  printBill: require("./lib/parser/printBill"),
+  tagBill: require("./lib/parser/tagBill"),
 
-  //Fields
+  /*
+    FIELDS
+  */
   validBillTypes: ["s", "hr"],
   validCongresses: [113, 114, 115]
 };
