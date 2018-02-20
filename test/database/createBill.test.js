7f267a9c6240b3ddbdbd12fb502cf6432c8a8652
jeffreyshen19 <jshen@andover.edu>
2018-02-20 14:44:00 -0500

    Daily Commit 2/20/18 Fixed the way I throw errors and added structure for database unit tests

--- test/database/createBill.test.js
+++ test/database/createBill.test.js
@@ -1,7 +1,16 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 var createBill = require("../../lib/database/createBill");
+var mongoose = require("mongoose");
 
 describe('createBill', function() {
-  
+  before(function(done){
+    mongoose.connect('mongodb://localhost/bills');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Error connecting to database'));
+    db.once('open', function() {
+      console.log('Connected to database');
+      done();
+    });
+  });
 });
--- test/database/generateDB.test.js
+++ test/database/generateDB.test.js
@@ -2,9 +2,17 @@
 
 var assert = require('chai').assert;
 var expect = require('chai').expect;
-var cleanBillStatus = require("../../lib/parser/cleanBillStatus");
-var getBillStatus = require("../../lib/scraper/getBillStatus");
+var cleanBillStatus = require("../../lib/database/generateDB");
+var mongoose = require("mongoose");
 
 describe('generateDB', function() {
-
+  before(function(done){
+    mongoose.connect('mongodb://localhost/bills');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Error connecting to database'));
+    db.once('open', function() {
+      console.log('Connected to database');
+      done();
+    });
+  });
 });
