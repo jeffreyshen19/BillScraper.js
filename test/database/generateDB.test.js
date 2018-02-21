23c76a79e75db43437c0d2119fa1865484fd22a2
jeffreyshen19 <jshen@andover.edu>
2018-02-21 12:37:24 -0500

    added unit tests for generateDB

--- test/database/generateDB.test.js
+++ test/database/generateDB.test.js
@@ -2,7 +2,7 @@
 
 var assert = require('chai').assert;
 var expect = require('chai').expect;
-var cleanBillStatus = require("../../lib/database/generateDB");
+var generateDB = require("../../lib/database/generateDB");
 var mongoose = require("mongoose");
 
 describe('generateDB', function() {
@@ -14,5 +14,27 @@ describe('generateDB', function() {
       console.log('Connected to database');
       done();
     });
+  });
+
+  it('should throw an error if type is not a valid bill type', function(){
+    expect(() => {
+      generateDB("random-text", 115, 1);
+    }).to.throw(TypeError);
+  });
+
+  it('should throw an error if congress is not a valid congress', function() {
+    expect(() => {
+      generateDB("s", 98, 1);
+    }).to.throw(TypeError);
+  });
+
+  it('should throw an error if session is not 1 or 2', function() {
+    expect(() => {
+      generateDB("s", 115, 3);
+    }).to.throw(TypeError);
+  });
+
+  it('should generate a database', function() {
+    generateDB("s", 115, 1);
   });
 });
